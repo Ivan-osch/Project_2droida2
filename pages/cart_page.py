8e97ef31489cc542ddb42d759fdc2fd96dd9f77e
Ivan-osch <vanes-oschepkov@mail.ru>
2025-04-07 17:19:25 +0700

    05

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -7,6 +7,7 @@ class CartPage(BasePage):
         name_in_cart_page = self.browser.find_element(*CartPageLocators.PRODUCT_NAME).text
         assert name_in_product_page == name_in_cart_page, "Имя товара в корзине не соответствует"
 
+
     def product_price_in_cart(self, price_in_product_page):
         price_in_cart_page = self.browser.find_element(*CartPageLocators.PRODUCT_PRICE).text
         assert price_in_product_page == price_in_cart_page, "Цена товара в корзине не соответствует"
